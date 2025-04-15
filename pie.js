09e8b3dd85364bce39fb1f922b54db1dbf179fda
Gwen Tripet-Costet <gwenden31@gmail.com>
2025-04-15 13:50:43 +0200

    Revert "VA-339 fix pie (#64)"
    This reverts commit e26d6600e5cc44d7a31ce0f5707a32f17ddafe81.

--- pie.js
+++ pie.js
@@ -16,19 +16,13 @@ H5P.Chart.PieChart = (function () {
       .range(["#90EE90", "#ADD8E6", "#FFB6C1", "#B0C4DE", "#D3D3D3", "#20B2AA", "#FAFAD2"]);
 
     // Create SVG
-    var viewBoxSize = 400;
-    var padding = 20;
-    var radius = (viewBoxSize / 2) - padding;
-
     var svg = d3.select($wrapper[0])
-      .append("svg")
-      .attr("viewBox", "0 0 " + viewBoxSize + " " + viewBoxSize)
+      .append("svg");
 
     svg.append("desc").html(params.figureDefinition);
 
     var translater = svg.append("g")
-      .attr("class", "translater")
-      .attr("transform", "translate(" + viewBoxSize / 2 + "," + viewBoxSize / 2 + ")");
+      .attr("class", "translater");
 
     var pie = d3.layout.pie()
       .sort(null)
@@ -41,15 +35,13 @@ H5P.Chart.PieChart = (function () {
       .enter().append("g")
       .attr("class", "arc");
 
-    var arcGenerator = d3.svg.arc()
-      .outerRadius(radius)
-      .innerRadius(0);
-
-    arcs.append("path")
+    var paths = arcs.append("path")
       .style("fill", function(d) {
-        return d.data.color !== undefined ? d.data.color : defColors(dataSet.indexOf(d.data) % defColors.range().length);
-      })
-      .attr("d", arcGenerator);
+        if (d.data.color !== undefined) {
+          return d.data.color;
+        }
+        return defColors(dataSet.indexOf(d.data) % 7);
+      });
 
     var texts = arcs.append("svg:text")
       .attr("class", "text")
@@ -62,11 +54,7 @@ H5P.Chart.PieChart = (function () {
         if (d.data.fontColor !== undefined) {
           return d.data.fontColor;
         }
-      })
-      .attr("transform", function(d) {
-        return "translate(" + arcGenerator.centroid(d) + ")";
-      })
-      .style("font-size", Math.max(radius * 0.1, 10) + "px");
+      });
 
     /**
      * Fit the current chart to the size of the wrapper.
@@ -74,18 +62,28 @@ H5P.Chart.PieChart = (function () {
     self.resize = function () {
       // Scale to smallest value of height and width
       var style = window.getComputedStyle($wrapper[0]);
-      var width = parseFloat(style.width);
-      var height = parseFloat(style.height);
-      var minDimension = Math.min(width, height);
-      var scalingFactor = minDimension / viewBoxSize;
-      var newRadius = (viewBoxSize / 2) - padding;
+      var scaleTo = Math.min(parseFloat(style.width), parseFloat(style.height));
 
-      var newFontSize = Math.max(newRadius * 0.1 * scalingFactor, 10);
-      texts.style("font-size", newFontSize + "px");
+      // Do the math
+      var width = scaleTo;
+      var height = scaleTo;
+      var padding = 0;
+      var radius = Math.min(width, height) / 2;
+      var arc = d3.svg.arc()
+        .outerRadius(radius - padding)
+        .innerRadius(0);
+
+      // Update positions
+      svg.attr('width', width + 'px')
+        .attr('height', height + 'px');
+      translater.attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")");
+      paths.attr("d", arc);
+      texts.attr("transform", function(d) {
+        d.innerRadius = 0;
+        d.outerRadius = radius - padding;
+        return "translate(" + arc.centroid(d) + ")";
+      });
     };
-
-    self.resize();
-    window.addEventListener('resize', self.resize);
   }
 
   return PieChart;
